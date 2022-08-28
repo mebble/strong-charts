07a4805205729cb4438d13badaf83a2641d8f8af
Neil Syiemlieh <neilsyiemlieh@gmail.com>
2022-08-28 11:06:22 +0530

    basic chart

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
-import { ChangeEventHandler, useState } from 'react'
+import { ChangeEventHandler, useState, useRef } from 'react'
 import { parse } from 'papaparse';
+import * as Plot from '@observablehq/plot';
 import reactLogo from './assets/react.svg'
 import './App.css'
 
@@ -18,26 +19,33 @@ type StrongLineItem = {
   'Workout Notes': string | null,
 };
 
-const handleFile: ChangeEventHandler = (e) => {
-  const input = e.target as HTMLInputElement;
-  if (!input.files) return;
-  const file = input.files[0];
-  if (file.type !== 'text/csv') {
-    console.error('Not a CSV file')
-    return;
-  };
-  parse<StrongLineItem>(file, {
-    header: true,
-    skipEmptyLines: true,
-    dynamicTyping: true,
-    complete(results) {
-      console.log(results)
-    },
-  })
-};
-
 function App() {
   const [count, setCount] = useState(0)
+  const chartRef = useRef<HTMLDivElement>(null);
+
+  const handleFile: ChangeEventHandler = (e) => {
+    const input = e.target as HTMLInputElement;
+    if (!input.files) return;
+    const file = input.files[0];
+    if (file.type !== 'text/csv') {
+      console.error('Not a CSV file')
+      return;
+    };
+    parse<StrongLineItem>(file, {
+      header: true,
+      skipEmptyLines: true,
+      dynamicTyping: true,
+      complete(results) {
+        console.log(results)
+        const plotSvg = Plot.plot({
+          marks: [
+            Plot.dot(results.data, { x: 'Date', y: 'Weight' })
+          ]
+        })
+        chartRef.current?.append(plotSvg)
+      },
+    })
+  };
 
   return (
     <div className="App">
@@ -58,6 +66,7 @@ function App() {
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
         <input type="file" id="input" onChange={handleFile} />
+        <div className="chart-container" ref={chartRef}></div>
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
---
+++ src/untyped.d.ts
@@ -0,0 +1,3 @@
+// https://stackoverflow.com/a/50516783
+// https://github.com/observablehq/plot/issues/401
+declare module '@observablehq/plot'
